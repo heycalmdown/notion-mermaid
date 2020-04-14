0b5efa1512ff0b5bb216c6fa42f40709285ef7e3
heycalmdown <heyacct@gmail.com>
2020-04-14 15:32:01 +0900

    feat: markmap 적용

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import { createAgent } from 'notionapi-agent';
 import _ from 'lodash';
 
+const { transform } = require('markmap-lib/dist/transform.common');
+
 const { NOTION_TOKEN } = process.env;
 
 export const router = express.Router();
@@ -59,7 +61,48 @@ router.get('/:blockid([0-9a-f]{32})', async (req, res) => {
     if (!sourceBlock) return res.send('invalid blockId ' + req.params.blockid);
     const text = (sourceBlock.value as any).properties.title[0][0];
     const text2 = text.replace(/\n/g, '\\n');
+    console.log(text2);
     res.render('index', { graphDefinition: text2 });
+  } catch (e) {
+    return res.send(e.message);
+  }
+});
+
+router.get('/mermaid/:blockid([0-9a-f]{32})', async (req, res) => {
+  const blockId = toDashID(req.params.blockid);
+  try {
+    const result = await notion.loadPageChunk({
+      pageId: blockId,
+      limit: 10,
+      cursor: { stack: [] },
+      chunkNumber: 0,
+      verticalColumns: false
+    });
+    const sourceBlock = result.recordMap.block[blockId];
+    if (!sourceBlock) return res.send('invalid blockId ' + req.params.blockid);
+    const text = (sourceBlock.value as any).properties.title[0][0];
+    const text2 = text.replace(/\n/g, '\\n');
+    console.log(text2);
+    res.render('index', { graphDefinition: text2 });
+  } catch (e) {
+    return res.send(e.message);
+  }
+});
+
+router.get('/markmap/:blockid([0-9a-f]{32})', async (req, res) => {
+  const blockId = toDashID(req.params.blockid);
+  try {
+    const result = await notion.loadPageChunk({
+      pageId: blockId,
+      limit: 10,
+      cursor: { stack: [] },
+      chunkNumber: 0,
+      verticalColumns: false
+    });
+    const sourceBlock = result.recordMap.block[blockId];
+    if (!sourceBlock) return res.send('invalid blockId ' + req.params.blockid);
+    const text = (sourceBlock.value as any).properties.title[0][0];
+    res.render('markmap', { markmapped: JSON.stringify(transform(text)) });
   } catch (e) {
     return res.send(e.message);
   }
